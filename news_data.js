e4895e618c61a77558e58b6fcb0edd68e292eb0e
sladee7766 <skulled4545@gmail.com>
2026-06-06 23:35:19 +0300

    Update news_data.js

--- news_data.js
+++ news_data.js
@@ -3,12 +3,11 @@ const newsData = [
         id: 1,
         type: "update",
         date: "2026-06-15",
-        title: { en: "Site Update: New Video Overlays", tr: "Site Güncellemesi: Yeni Video Arayüzü" },
+        title: { en: "Site Update: Professional Tactical UI", tr: "Site Güncellemesi: Profesyonel Taktiksel Arayüz" },
         thumbnail: "picture/logo.png",
         content: [
-            { type: "text", en: "We now live with the beta website.", tr: "beta web sitesi ile internete açıldık." },
-            { type: "video", url: "https://www.youtube.com/embed/dQw4w9WgXcQ" },
-            { type: "text", en: "see yall soon.", tr: "bizleri ileri günerde göreceksiniz." }
+            { type: "text", en: "BETA v1.0.0 is live with zooming, panning, and filters.", tr: "BETA v1.0.0; yakınlaştırma, kaydırma ve filtrelerle yayında." },
+            { type: "video", url: "https://www.youtube.com/embed/dQw4w9WgXcQ" }
         ]
     }
 ];
