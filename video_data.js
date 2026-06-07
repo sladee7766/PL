fc0f67c2d0333042cfad3f00d768e215b654203f
sladee7766 <skulled4545@gmail.com>
2026-06-07 16:12:00 +0300

    Add files via upload

---
+++ video_data.js
@@ -0,0 +1,10 @@
+const videoData = [
+    {
+        id: 1,
+        title: { en: "Top 10 Sova Lineups on Ascent", tr: "Ascent Haritasý En Ýyi 10 Sova Lineup'ý" },
+        thumbnail: "map_backgrounds/ascent.jpg",
+        url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        date: "2026-06-07",
+        duration: "10:24"
+    }
+];
