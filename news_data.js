b29666eed140463d56406a4b306c5f67cc75d333
sladee7766 <skulled4545@gmail.com>
2026-06-07 16:11:21 +0300

    Update news_data.js

--- news_data.js
+++ news_data.js
@@ -10,4 +10,15 @@ const newsData = [
             { type: "video", url: "https://www.youtube.com/embed/dQw4w9WgXcQ" }
         ]
     }
+        {
+        id: 2,
+        type: "update",
+        date: "2026-06-15",
+        title: { en: "Site Update: added the video tab and fixed the bugs", tr: "Site Güncellemesi: videolar eklentisi eklendi ve buglar düzeltildi" },
+        thumbnail: "picture/logo.png",
+        content: [
+            { type: "text", en: "Site Update: added the video tab and fixed the bugs", tr: "Site Güncellemesi: videolar eklentisi eklendi ve buglar düzeltildi" },
+            { type: "video", url: "https://www.youtube.com/embed/dQw4w9WgXcQ" }
+        ]
+    }
 ];
