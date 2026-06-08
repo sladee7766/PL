1f0c00dbb9f389f02e72a152dcd5165fa6494512
sladee7766 <skulled4545@gmail.com>
2026-06-08 17:11:24 +0300

    Add files via upload

---
+++ crosshairs_data.js
@@ -0,0 +1,5 @@
+const crosshairData = [
+    { player: "TenZ", team: "Sentinels", code: "0;s;1;P;c;5;h;0;m;1;0l;4;0v;2;0o;2;0a;1;0f;0;1b;0;S;c;4;o;1", image: "picture/logo.png" },
+    { player: "Demon1", team: "NRG", code: "0;s;1;P;o;1;f;0;0t;1;0l;3;0o;2;0a;1;0f;0;1b;0;S;c;1;o;1", image: "picture/logo.png" },
+    { player: "Boaster", team: "Fnatic", code: "0;s;1;P;c;1;o;1;d;1;0b;0;1b;0;S;c;1;o;1", image: "picture/logo.png" }
+];
